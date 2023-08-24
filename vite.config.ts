5b9cf89d21ee210fad09f416f5a94f82f5144e50
ssshooter <ssshooterx@gmail.com>
2023-08-24 22:22:56 +0800

    build: pack css into js

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), cssInjectedByJsPlugin()],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
